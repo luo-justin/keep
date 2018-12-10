17c18fb3e6fb0cb43369f5089126fda549d46a75
Justin Luo <justinluo1998@gmail.com>
2018-12-09 22:30:25 -0500

    added modal to create

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import Nav from './components/Nav';
 import Card from './components/Card';
 import Create from './components/Create';
 
-import Firebase from './Firebase';
+import firebase from './Firebase';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 
 class App extends Component {
-  render() {
-     document.addEventListener('DOMContentLoaded', function() {
+  constructor(props){
+    super(props);
+    this.ref = firebase.firestore().collection('boards');
+    this.initModal();
+  }
+
+  initModal() {
+    document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, []);
-  });
+    });
+  }
+
+  render() {
     return (
       <div class="">
         <Nav/>
--- src/components/Create.js
+++ src/components/Create.js
@@ -9,15 +9,31 @@ class Create extends Component{
 		};
 	}
 
+	onChange = (e) =>{
+		
+	}
+
 	render(){
 		return (
 			<div id="modal1" class="modal">
 		    <div class="modal-content">
-		      <h4>Modal Header</h4>
-		      <p>A bunch of text</p>
+		      <h4 style={{paddingBottom: "24px"}}>Create a Task</h4>
+		      <div class="row">
+		        <form class="col s12">
+				      <div class="input-field col s6">
+			          <input placeholder="Grocery shopping" id="first_name" type="text" class="validate"/>
+			          <label for="first_name">Title</label>
+		        	</div>
+		        	<div class="input-field col s6">
+			          <input placeholder="Buy two eggs" id="first_name" type="text" class="validate"/>
+			          <label for="first_name">Description</label>
+		        	</div>
+	        	</form>
+        	</div>
 		    </div>
 		    <div class="modal-footer">
-		      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+		      <a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
+		      <a href="#!" class="waves-effect waves-green btn-flat">Add Task</a>
 		    </div>
 		  </div>);
 	}
